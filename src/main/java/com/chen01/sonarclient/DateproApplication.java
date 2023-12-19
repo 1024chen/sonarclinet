eba7596b45b39d3d03656381df68b27c5f5ef18f
1024chen <chen0adapter@foxmail.com>
2023-12-19 18:39:20 +0800

    first commit

---
+++ src/main/java/com/chen01/sonarclient/DateproApplication.java
@@ -0,0 +1,13 @@
+package com.chen01.sonarclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DateproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DateproApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chen01/sonarclient/DateproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chen01.sonarclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DateproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
