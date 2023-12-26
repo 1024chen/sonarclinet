a5fd2bc3294b4b00b834e63134bb7fa55d1051de
1024chen <chen0adapter@foxmail.com>
2023-12-26 19:50:53 +0800

    加入有道api调用

--- src/test/java/com/chen01/sonarclient/service/TranslatorServiceTest.java
+++ src/test/java/com/chen01/sonarclient/service/TranslatorServiceTest.java
@@ -4,6 +4,9 @@ import jakarta.annotation.Resource;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -14,6 +17,8 @@ class TranslatorServiceTest {
 
     @Test
     void getTransResult() {
-        System.out.println(translatorService.getTransResult("test"));
+        List<String> wordList = Arrays.asList("test","touch me","just kidding",
+                "I use the java language to develop programs");
+        System.out.println(translatorService.getTransResult(wordList.toString()));
     }
 }
